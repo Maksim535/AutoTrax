8e255e1476cda6dc95d62c7120658a1e2c4ec496
Maksim <maksim.lakoseljac@gmail.com>
2026-05-28 00:07:57 +0200

    Initial project setup

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen bg-[#0f0f0f] flex items-center justify-center px-6">
+    <div
+      class="w-full max-w-md bg-[#171717] rounded-3xl p-8 shadow-2xl border border-[#2a2a2a]"
+    >
+    
+      <div class="mb-10 text-center">
+        <h1 class="text-5xl font-bold text-white">
+          Auto<span class="text-orange-500">Trax</span>
+        </h1>
+
+        <p class="text-gray-400 mt-3 text-sm">
+         Digitalna povijest servisa vozila
+        </p>
+      </div>
+
+      
+</template>
+
