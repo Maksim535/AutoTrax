eb55b8d5d6ac8a5e0f7eb06446a224e2198e7bd1
Maksim <maksim.lakoseljac@gmail.com>
2026-09-10 22:29:06 +0200

    Dodana Firestore baza i CRUD za vozila

---
+++ src/stores/dataStore.js
@@ -0,0 +1,98 @@
+import { defineStore } from 'pinia'
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc
+} from 'firebase/firestore'
+
+import { db } from '../firebase'
+
+export const useDataStore = defineStore('data', {
+  state: () => ({
+    vehicles: [],
+    services: [],
+    loading: false,
+    error: null
+  }),
+
+  actions: {
+
+    async addVehicle(vehicle) {
+      this.loading = true
+      this.error = null
+
+      try {
+        const docRef = await addDoc(
+          collection(db, 'vehicles'),
+          vehicle
+        )
+
+        this.vehicles.push({
+          id: docRef.id,
+          ...vehicle
+        })
+
+      } catch (error) {
+        this.error = error.message
+        throw error
+
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async getVehicles() {
+      this.loading = true
+      this.error = null
+
+      try {
+        const snapshot = await getDocs(
+          collection(db, 'vehicles')
+        )
+
+        this.vehicles = snapshot.docs.map((document) => ({
+          id: document.id,
+          ...document.data()
+        }))
+
+      } catch (error) {
+        this.error = error.message
+        throw error
+
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async updateVehicle(id, vehicle) {
+      const vehicleRef = doc(db, 'vehicles', id)
+
+      await updateDoc(vehicleRef, vehicle)
+
+      const index = this.vehicles.findIndex(
+        (vehicle) => vehicle.id === id
+      )
+
+      if (index !== -1) {
+        this.vehicles[index] = {
+          id,
+          ...vehicle
+        }
+      }
+    },
+
+    async deleteVehicle(id) {
+      await deleteDoc(
+        doc(db, 'vehicles', id)
+      )
+
+      this.vehicles = this.vehicles.filter(
+        (vehicle) => vehicle.id !== id
+      )
+    }
+
+  }
+})
